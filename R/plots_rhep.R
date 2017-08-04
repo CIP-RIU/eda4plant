6d5364daf81a99cace118400060a95a26d76a5b9
reyzaguirre <reyzaguirre@gmail.com>
2017-08-04 15:01:37 -0500

    ggplots

---
+++ R/plots_rhep.R
@@ -0,0 +1,49 @@
+# Dotplot
+# Plots replications by genotype
+# Recomended for up to 15 genotypes
+# Plots genotypes by environment. Two options:
+# 1. Original values
+# 2. Means over replications
+
+
+# Boxplot
+# Plots genotypes. Two options:
+# 1. Original values
+# 2. Means over replications
+# Plots genotypes by environment. Two options:
+# 1. Original values
+# 2. Means over replications
+
+
+# Histogram
+# Plots genotypes. Two options:
+# 1. Original values
+# 2. Means over replications
+# Plots genotypes by environment. Two options:
+# 1. Original values
+# 2. Means over replications
+
+
+# Density plot
+# Plots genotypes. Two options:
+# 1. Original values
+# 2. Means over replications
+# Plots genotypes by environment. Two options:
+# 1. Original values
+# 2. Means over replications
+
+
+# AMMI and GGE
+# Only for MET data
+
+
+# Scatterplot
+# Plots two traits.
+# Options:
+# 1. Original values.
+# 2. Means over replications.
+# 3. Means over replications and environments.
+# With facets:
+# One facet for each environment
+# With several traits:
+# Matrix scatterplot
