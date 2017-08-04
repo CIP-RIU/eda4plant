1965b0aa6c67f669cfb96e2fcd3ee7cbd54b55e4
reyzaguirre <reyzaguirre@gmail.com>
2017-08-04 16:21:11 -0500

    hist

--- R/plots_rhep.R
+++ R/plots_rhep.R
@@ -1,3 +1,13 @@
+# Fake data for examples
+
+env <- gl(6, 100)
+geno <- rep(gl(50, 2), 6)
+rep <- rep(gl(2, 1), 300)
+
+fb <- data.frame(env = env, geno = geno, rep = rep)
+
+fb$y <- rnorm(600, 100 + as.numeric(geno) * rnorm(1) + as.numeric(env))
+
 # Dotplot
 # Plots replications by genotype
 # Recomended for up to 15 genotypes
@@ -5,6 +15,11 @@
 # 1. Original values
 # 2. Means over replications
 
+plot_dot <- function() {
+
+  ggplot(fb)
+  
+}
 
 # Boxplot
 # Plots genotypes. Two options:
@@ -14,6 +29,11 @@
 # 1. Original values
 # 2. Means over replications
 
+plot_box <- function() {
+  
+  ggplot(fb)
+  
+}
 
 # Histogram
 # Plots genotypes. Two options:
@@ -22,6 +42,15 @@
 # Plots genotypes by environment. Two options:
 # 1. Original values
 # 2. Means over replications
+
+plot_hist <- function(trait, bins, fb) {
+  
+  ggplot(fb) +
+    geom_histogram(aes_string(trait), bins = bins, col = 1, alpha = 0.5)
+
+}
+
+plot_hist("y", 20, fb)
 
 
 # Density plot
@@ -32,10 +61,20 @@
 # 1. Original values
 # 2. Means over replications
 
+plot_dens <- function() {
+  
+  ggplot(fb)
+  
+}
 
 # AMMI and GGE
 # Only for MET data
 
+plot_ammi <- function() {
+  
+  ggplot(fb)
+  
+}
 
 # Scatterplot
 # Plots two traits.
@@ -47,3 +86,10 @@
 # One facet for each environment
 # With several traits:
 # Matrix scatterplot
+
+plot_scat <- function() {
+  
+  ggplot(fb)
+  
+}
+
